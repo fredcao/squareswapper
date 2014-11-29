64c4b3857c84fe7572f8699e5653d852ace89995
KevinChenXue <kevinxue122@gmail.com>
2014-11-29 14:15:58 -0500

    Added match functions

--- boardinterpreter.cc
+++ boardinterpreter.cc
@@ -188,55 +188,171 @@ bool BoardInterpreter::textOnly() {
 void BoardInterpreter::match3() {
 
 // Find horizontal and vertical matches of 3
-// Notify the squares that are part of the match
-// Call doEffect(sq) for each square that are part of the match
-	
-	int curColour;
-	for(int i=0;i < boardSize; i++){	//checking horizontal matches
-		for(int j=0;j < boardSize-2; j++){
-			if(!(board[i][j]&&board[i][j+1]&&board[i][j+2])){	//checking that not null
-				continue;
-			}
-			curColour=board[i][j]->getColour();
-			if(board[i][j+1]->getColour()==curColour && board[i][j+2]->getColour()==curColour){
-				board[i][j]->notify();
-				board[i][j+1]->notify();
-				board[i][j+2]->notify();
-				j+=3;
-			}
-		}
-	}
-	for(int j=0;j<boardSize;j++){		//checking vertical matches
-		for(int i=0;i<boardSize-2;i++){
-			if(!(board[i][j]&&board[i+1][j]&&board[i+2][j])){
-				continue;
-			}
-			curColour=board[i][j]->getColour();
-			if(board[i+1][j]->getColour()==curColour && board[i+2][j]->getColour()==curColour){
-				board[i][j]->notify();
-				board[i+1][j]->notify();
-				board[i+2][j]->notify();
-				i+=3;
-			}
-		}
-	}
+ // Notify the squares that are part of the match
+ // Call doEffect(sq) for each square that are part of the match
+
+         int curColour;
+         for(int i=0;i < boardSize; i++){        //checking horizontal matches
+                 for(int j=0;j < boardSize-2; j++){
+                         if(!(board[i][j]&&board[i][j+1]&&board[i][j+2])){           //checking that not null
+                                 continue;
+                         }
+                         curColour=board[i][j]->getColour();
+                         if(board[i][j+1]->getColour()==curColour && board[i][j+2]->getColour()==curColour){
+                                 if(board[i][j])doEffect(board[i][j],3);
+                                 if(board[i][j+1])doEffect(board[i][j+1],3);
+                                 if(board[i][j+2])doEffect(board[i][j+2],3); 
+                                 j+=3;
+                         }
+                 }
+         }
+         for(int j=0;j<boardSize;j++){           //checking vertical matches
+                 for(int i=0;i<boardSize-2;i++){
+                         if(!(board[i][j]&&board[i+1][j]&&board[i+2][j])){
+                                 continue;
+                         }
+                         curColour=board[i][j]->getColour();
+                         if(board[i+1][j]->getColour()==curColour && board[i+2][j]->getColour()==curColour){
+                                 if(board[i][j])doEffect(board[i][j],3);
+                                 if(board[i+1][j])doEffect(board[i+1][j],3);
+                                 if(board[i+2][j])doEffect(board[i+2][j],3);
+                                 i+=3;
+                         }
+                 }
+         }
 }
 
 void BoardInterpreter::match4() {
 
 // Same thing as match3 except with 4
-
+	int curColour;
+        for(int i=0;i<boardSize;i++){
+                for(int j=0;j<boardSize-3;j++){
+                        if(!(board[i][j]&&board[i][j+1]&&board[i][j+2]&&board[i][j+3])){
+                                continue;
+                        }
+                        curColour=board[i][j]->getColour();
+                        if(board[i][j+1]->getColour()==curColour && board[i][j+2]->getColour()==curColour && board[i][j+3]->getColour()==curColour){
+                                if(board[i][j])doEffect(board[i][j],5);
+                                if(board[i][j+1])doEffect(board[i][j+1],5);
+                                if(board[i][j+2])doEffect(board[i][j+2],5);
+                                if(board[i][j+3])doEffect(board[i][j+3],5);
+                                j+=4;
+                        }
+                }
+        }
+        for(int j=0;j<boardSize;j++){
+                for(int i=0;i<boardSize-3;i++){
+                        if(!(board[i][j]&&board[i+1][j]&&board[i+2][j]&&board[i+3][j])){
+                                continue;
+                        }
+                        curColour=board[i][j]->getColour();
+                        if(board[i+1][j]->getColour()==curColour && board[i+2][j]->getColour()==curColour && board[i+3][j]->getColour()==curColour){
+                                if(board[i][j])doEffect(board[i][j],5);
+                                if(board[i+1][j])doEffect(board[i+1][j],5);
+                                if(board[i+2][j])doEffect(board[i+2][j],5);
+                                if(board[i+3][j])doEffect(board[i+3][j],5);
+                                i+=4;
+                        }
+                }
+        }
 }
 
 void BoardInterpreter::match5() {
 
 // Same thing as match4 except...
 // Really this is match 5 or more as matching for example 10 gives you the same result
-
+	 int curColour;
+        int count = 1;
+        for(int i=0;i<boardSize;i++){
+                for(int j=0;j<boardSize-4;j++){         //horizontal
+                        if(board[i][j]){
+                                curColour==board[i][j]->getColour();
+                                for(int k=j+1;k<boardSize;k++){
+                                        if(!(board[i][k]&&board[i][k]->getColour()==curColour)){
+                                                break;
+                                        }
+                                        count++;
+                                }
+                                if(count>=5){           //minimum 5 in  row
+                                        for(int k=j;k<j+count;k++){
+                                                if(board[i][k])doEffect(board[i][k],5);
+                                        }
+                                        j+=5;
+                                }
+                                count=1;
+                        }
+                }
+        }
+        for(int j=0;j<boardSize;j++){
+                for(int i=0;i<boardSize-4;i++){
+                        if(board[i][j]){
+                                curColour=board[i][j]->getColour();
+                                for(int k=i+1;k<boardSize;k++){
+                                        if(!(board[k][j]&&board[k][j]->getColour()==curColour)){
+                                                break;
+                                        }
+                                        count++;
+                                }
+                                if(count>=5){
+                                        for(int k=i;k<i+count;k++){
+                                                if(board[k][j])doEffect(board[k][j],5);
+                                        }
+                                        i+=5;
+				 }
+                                count=1;
+                        }
+                }
+        }
 }
 
 void BoardInterpreter::matchL() {
 
-// Same thing as match5 except actually an L instead of line
-
+	int curColour;  // Same thing as match5 except actually an L instead of line
+        for(int i=0;i<boardSize;i++){
+                for(int j=0;j<boardSize-2;j++){
+                        if(!(board[i][j]&&board[i][j+1]&&board[i][j+2])){
+                                continue;
+                        }
+                        curColour=board[i][j]->getColour();
+                        if(board[i][j+1]->getColour()==curColour&&board[i][j+2]->getColour()==curColour){       //found horizontal match of 3
+                                if(i>1){
+                                        if(board[i-1][j]&&board[i-2][j]&&board[i-1][j]->getColour()==curColour&&board[i-2][j]->getColour()==curColour){
+                                                if(board[i][j])doEffect(board[i][j],5);
+                                                if(board[i][j+1])doEffect(board[i][j+1],5);
+                                                if(board[i][j+2])doEffect(board[i][j+2],5);
+                                                if(board[i-1][j])doEffect(board[i-1][j],5);
+                                                if(board[i-2][j])doEffect(board[i-2][j],5);
+                                                j+=3;
+                                        }
+                                        else if(board[i-1][j+2]&&board[i-2][j+2]&&board[i-1][j+2]->getColour()==curColour&&board[i-2][j+2]->getColour()==curColour){
+                                                if(board[i][j])doEffect(board[i][j],5);
+                                                if(board[i][j+1])doEffect(board[i][j+1],5);
+                                                if(board[i][j+2])doEffect(board[i][j+2],5);
+                                                if(board[i-1][j+2])doEffect(board[i-1][j+2],5);
+                                                if(board[i-2][j+2])doEffect(board[i-2][j+2],5);
+                                                j+=3;
+                                        }
+                                }
+				 if(i<8){
+                                        if(board[i+1][j]&&board[i+2][j]&&board[i+1][j]->getColour()==curColour&&board[i+2][j]->getColour()==curColour){
+                                                if(board[i][j])doEffect(board[i][j],5);
+                                                if(board[i][j+1])doEffect(board[i][j+1],5);
+                                                if(board[i][j+2])doEffect(board[i][j+2],5);
+                                                if(board[i+1][j])doEffect(board[i+1][j],5);
+                                                if(board[i+2][j])doEffect(board[i+2][j],5);
+                                                j+=3;
+                                        }
+                                        else if(board[i+1][j+2]&&board[i+2][j+2]&&board[i+1][j+2]->getColour()==curColour&&board[i+2][j+2]->getColour()==curColour){
+                                                if(board[i][j])doEffect(board[i][j],5);
+                                                if(board[i][j+1])doEffect(board[i][j+1],5);
+                                                if(board[i][j+2])doEffect(board[i][j+2],5);
+                                                if(board[i+1][j+2])doEffect(board[i+1][j+2],5);
+                                                if(board[i+2][j+2])doEffect(board[i+2][j+2],5);
+                                                j+=3;
+                                        }
+                                }
+                        }
+                }
+        }
 }
